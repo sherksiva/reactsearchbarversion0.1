9b1fed84c4dcc2781f0e5d3c28fa7d1be3656856
SivaprakashRaman <prakashraman441@gmail.com>
2025-11-05 06:05:29 +0530

    Css design and alignment css updated

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,12 +1,23 @@
 import "./SearchResult.css";
 
-export const SearchResult = ({ result }) => {
+export const SearchResult = ({ result, username }) => {
   return (
-    <div
+	<div class="card" onClick={(e) => alert(`You selected ${result}!`)}>
+      <div class="face face1">
+        <div class="content">
+          <p>{username}</p> 
+          
+        </div>
+      </div>
+      <div class="face face2">
+        <p class="content2">{result}</p>
+      </div>
+    </div>
+    /*<div
       className="search-result results-list-manual"
       onClick={(e) => alert(`You selected ${result}!`)}
     >
       {result}
-    </div>
+    </div>*/
   );
 };
